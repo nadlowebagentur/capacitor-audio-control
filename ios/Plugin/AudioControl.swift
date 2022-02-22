826f36e20895c935509d3c1d3aee78ac8a19d9a8
Leonid Marchenko <codobred@gmail.com>
2022-02-22 14:22:33 +0200

    refactor(IOS): remove unneded import

--- ios/Plugin/AudioControl.swift
+++ ios/Plugin/AudioControl.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 import MediaPlayer
-import UIKit
 
 @objc public class AudioControl: NSObject {
     private let volumeView = MPVolumeView(frame: .zero)
-    
+
     @objc public func setVolume(_ value: Float) -> Float {
         guard let slider = volumeView.subviews.compactMap({ $0 as? UISlider }).first else {
             return value
         }
-        
+
         slider.setValue(value, animated: true)
-        
+
         return slider.value
     }
 }
